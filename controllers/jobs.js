9a6de680b073d7c7b1a8653ff553035281636c2f
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-05-19 22:52:48 +0530

    feat:Create and getAll jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,18 @@
+const { StatusCodes } = require("http-status-codes");
+const Job = require("../models/Job");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs });
 };
 const getJob = async (req, res) => {
-  res.send("get  jobs");
+  
+  res.send("get Single jobs");
 };
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  return res.status(StatusCodes.CREATED).json({ job });
 };
 const updateJob = async (req, res) => {
   res.send("update jobs");
